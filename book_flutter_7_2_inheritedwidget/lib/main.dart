2c1bcebabcc029fb1c4f03d3454ad0df56117cbc
ko2ic <ko2ic.dev@gmail.com>
2020-11-14 02:00:48 +0900

    7章inheritedwidgetを追加

---
+++ book_flutter_7_2_inheritedwidget/lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView(
+        children: <Widget>[
+          // TODO それぞれのアーキテクチャ用の画面を追加していく
+          ListTile(
+            title: const Text("InheritedWidgetの場合"),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TopPage1(),
+                    fullscreenDialog: true,
+                  ));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TopPage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _HomePage(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('InheritedWidget Demo'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _WidgetA(),
+            _WidgetB(),
+            _WidgetC(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _HomePage extends StatefulWidget {
+  _HomePage({
+    Key key,
+    this.child,
+  }) : super(key: key);
+  final Widget child;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+
+  static _HomePageState of(BuildContext context, {bool rebuild = true}) {
+    if (rebuild) {
+      return (context.dependOnInheritedWidgetOfExactType<_MyInheritedWidget>()).data;
+    }
+    return (context.getElementForInheritedWidgetOfExactType<_MyInheritedWidget>().widget as _MyInheritedWidget).data;
+  }
+}
+
+class _HomePageState extends State<_HomePage> {
+  int counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _MyInheritedWidget(
+      data: this,
+      child: widget.child,
+    );
+  }
+}
+
+class _MyInheritedWidget extends InheritedWidget {
+  _MyInheritedWidget({
+    Key key,
+    @required Widget child,
+    @required this.data,
+  }) : super(key: key, child: child);
+  final _HomePageState data;
+
+  @override
+  bool updateShouldNotify(_MyInheritedWidget oldWidget) {
+    return true;
+  }
+}
+
+class _WidgetA extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("called _WidgetA#build()");
+// 1 Stateの取得方法
+    final state = _HomePage.of(context);
+    return Center(
+      child: Text(
+        '${state.counter}',
+        style: Theme.of(context).textTheme.headline4,
+      ),
+    );
+  }
+}
+
+class _WidgetB extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("called _WidgetB#build()");
+    return const Text('I am a widget that will not be rebuilt.');
+  }
+}
+
+class _WidgetC extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("called _WidgetC#build()");
+// 2 こっちはrebuildしない
+    final state = _HomePage.of(context, rebuild: false);
+    return RaisedButton(
+      onPressed: () {
+        state._incrementCounter();
+      },
+      child: const Icon(Icons.add),
+    );
+  }
+}
