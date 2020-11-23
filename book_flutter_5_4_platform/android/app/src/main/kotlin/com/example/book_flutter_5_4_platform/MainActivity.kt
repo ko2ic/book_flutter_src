5b70efb299c56937ddc9121fbedb7f9c5c17d30c
ko2ic <ko2ic.dev@gmail.com>
2020-11-23 12:45:40 +0900

    stable, 1.22.4時点での書き方に修正

--- book_flutter_5_4_platform/android/app/src/main/kotlin/com/example/book_flutter_5_4_platform/MainActivity.kt
+++ book_flutter_5_4_platform/android/app/src/main/kotlin/com/example/book_flutter_5_4_platform/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.book_flutter_5_4_platform
 
 import android.content.Intent
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
+
 
 class MainActivity : FlutterActivity() {
 
@@ -16,25 +17,25 @@ class MainActivity : FlutterActivity() {
 
     lateinit var channel: MethodChannel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-
-        this.channel = MethodChannel(flutterView, CHANNEL)
-
-        channel.setMethodCallHandler { call, result ->
-            when (call.method) {
-                "toPlatformScreen" -> {
-                    val label = call.argument<String>("label_from_dart")
-                    startActivityForResult(NextActivity.intent(this@MainActivity).apply {
-                        putExtra("label", label)
-                    }, REQUEST_CODE)
-                    result.success("Platform Screen was displayed!!")
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        this.channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).apply {
+            setMethodCallHandler { call: MethodCall?, result: MethodChannel.Result? ->
+                when (call?.method) {
+                    "toPlatformScreen" -> {
+                        val label = call.argument<String>("label_from_dart")
+                        startActivityForResult(NextActivity.intent(this@MainActivity).apply {
+                            putExtra("label", label)
+                        }, REQUEST_CODE)
+                        result?.success("Platform Screen was displayed!!")
+                    }
+                    else -> result?.notImplemented()
                 }
-                else -> result.notImplemented()
             }
         }
+
     }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
