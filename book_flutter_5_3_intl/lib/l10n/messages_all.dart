2479b2f3efd5f6fdfc59f6687c17b47976b7d814
ko2ic <ko2ic.dev@gmail.com>
2020-11-23 17:20:34 +0900

    intl_translation: ^0.17.10+1にバージョンアップ

--- book_flutter_5_3_intl/lib/l10n/messages_all.dart
+++ book_flutter_5_3_intl/lib/l10n/messages_all.dart
@@ -15,8 +15,8 @@ import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
 import 'package:intl/src/intl_helpers.dart';
 
-import 'messages_ja.dart' as messages_ja;
 import 'messages_messages.dart' as messages_messages;
+import 'messages_ja.dart' as messages_ja;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
@@ -37,7 +37,10 @@ MessageLookupByLibrary _findExact(String localeName) {
 
 /// User programs should call this before using [localeName] for messages.
 Future<bool> initializeMessages(String localeName) async {
-  var availableLocale = Intl.verifiedLocale(localeName, (locale) => _deferredLibraries[locale] != null, onFailure: (_) => null);
+  var availableLocale = Intl.verifiedLocale(
+    localeName,
+    (locale) => _deferredLibraries[locale] != null,
+    onFailure: (_) => null);
   if (availableLocale == null) {
     return new Future.value(false);
   }
@@ -57,7 +60,8 @@ bool _messagesExistFor(String locale) {
 }
 
 MessageLookupByLibrary _findGeneratedMessagesFor(String locale) {
-  var actualLocale = Intl.verifiedLocale(locale, _messagesExistFor, onFailure: (_) => null);
+  var actualLocale = Intl.verifiedLocale(locale, _messagesExistFor,
+      onFailure: (_) => null);
   if (actualLocale == null) return null;
   return _findExact(actualLocale);
 }
